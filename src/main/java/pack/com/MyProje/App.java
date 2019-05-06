bbd6f6936e98afe1a1a34fb6d703aa74b9ce06b7
sha567 <ganganashanthi@gmail.com>
2019-05-06 19:53:28 +0530

    starting commit

---
+++ src/main/java/pack/com/MyProje/App.java
@@ -0,0 +1,13 @@
+package pack.com.MyProje;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
