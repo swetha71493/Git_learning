315bae9cbd3cba2badff9ea1b0f6a819242b627d
sha567 <ganganashanthi@gmail.com>
2019-05-07 13:09:46 +0530

    this is commit

---
+++ src/test/java/pack/com/MyProje/EditingGit.java
@@ -0,0 +1,11 @@
+package pack.com.MyProje;
+
+public class EditingGit {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.println("im editing");
+	}
+
+}
